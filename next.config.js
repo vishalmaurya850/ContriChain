deb896620ecb5a12ca698e39932fb52a82e21693
Vishal Maurya <vishalmaurya850@gmail.com>
2025-03-23 10:26:16 +0530

    Solved MongoDB build errors

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          net: false,
-          tls: false,
-          fs: false,
-          child_process: false,
-        };
-      }
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;  
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        net: false,
+        tls: false,
+        fs: false,
+        child_process: false,
+        'timers/promises': false,
+        dns: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
